237849815a96fa04b0f8103e96ffe2440b106b86
konstantiner <kastyan0403@mail.ru>
2022-02-19 20:53:30 +0300

    7,2
    После 7 урока

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Article struct {
+	Id uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+
 func index(w http.ResponseWriter, r *http.Request){
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 
@@ -16,7 +23,27 @@ func index(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, err.Error())
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	db, err := sql.Open("mysql", "mysql:mysql@tcp(127.0.0.1:3306)/golang")
+	if err != nil{
+		panic(err)
+	}
+	defer db.Close()
+// Выборка данных
+	res, err := db.Query("SELECT * FROM `articles`")
+	if err != nil {
+		panic(err)
+	}
+	posts = []Article{}
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err !=nil {
+			panic(err)
+		}
+		posts = append(posts, post)
+	}
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func create(w http.ResponseWriter, r *http.Request){
@@ -34,6 +61,9 @@ func save_article(w http.ResponseWriter, r *http.Request){
 	anons := r.FormValue("anons")
 	full_text := r.FormValue("full_text")
 
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
 	db, err := sql.Open("mysql", "mysql:mysql@tcp(127.0.0.1:3306)/golang")
 	if err != nil{
 		panic(err)
@@ -41,13 +71,14 @@ func save_article(w http.ResponseWriter, r *http.Request){
 	defer db.Close()
 
 	//Добавление данных
-	insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES('&s', '&s', '&s')", title, anons, full_text))
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES('%s', '%s', '%s')", title, anons, full_text))
 	if err != nil {
 		panic(err)
 	}
 	defer insert.Close()
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
 }
 
 func handleFunc() {
